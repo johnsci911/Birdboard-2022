31634464b22f8f4635908950a85d37dccac22848
johnsci911 <johnguitarizta@gmail.com>
2022-05-19 06:29:59 +0800

    Install laravel-ide-helper and initial unit test

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
+use App\Http\Controllers\ProjectsController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/projects', [ProjectsController::class, 'index']);
+
+Route::post('/projects', [ProjectsController::class, 'store']);
