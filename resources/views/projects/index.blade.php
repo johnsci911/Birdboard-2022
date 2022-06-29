b3c7f67432416b4a4dd8560cf66147c1502adbcf
johnsci911 <johnguitarizta@gmail.com>
2022-06-30 03:56:25 +0800

    A decent betterer layout :)

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,19 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="flex items-center mb-3">
-    <a href="/projects/create">New Project</a>
-  </div>
+  <header class="flex items-center mb-3 py-4">
+		<div class="flex justify-between items-center w-full">
+			<h3 class="text-grey no-underline text-sm font-normal">My projects</h3>
+			<a href="/projects/create" class="button">New Project</a>
+		</div>
+  </header>
 
-  <div class="flex">
+  <main class="lg:flex lg:flex-wrap -mx-3">
     @forelse ($projects as $project)
-      <div class="mr-4 p-5 bg-white rounded shadow w-1/3" style="height: 200px;">
-        <h3 class="mb-4 font-normal text-xl py-4">{{ $project->title }}</h3>
-        <div class="text-grey">{{ str_limit($project->description, 250) }}</div>
-      </div>
+      <div class="lg:w-1/3 px-3 pb-6">
+				<div class="p-5 bg-white rounded-lg shadow" style="height: 200px;">
+					<h3 class="font-normal text-xl py-4 -ml-5 border-l-4 border-blue-light pl-4 mb-3">
+						<a href="{{ $project->path() }}" class="text-black no-underline">{{ $project->title }}</a>
+					</h3>
+					<div class="text-grey">{{ str_limit($project->description, 250) }}</div>
+				</div>
+		  </div>
     @empty
       <div>No projects yet</div>
     @endforelse
-  </div>
+  </main>
 
 @endsection
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ let colors = {
 	...defaultColors,
 	'grey': 'rgba(0, 0, 0, 0.4)',
   'grey-light': '#F5F6F9',
+  'blue': '#47cdff',
+  'blue-light': '#8ae2fe'
 }
 
 module.exports = {
