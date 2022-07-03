26cce7c9eb4484cd30ddb9dcb7688724be80d37b
johnsci911 <johnguitarizta@gmail.com>
2022-07-04 01:10:54 +0800

    Reuse project form from create and edit, a little cleanup

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -23,22 +23,7 @@ class ProjectsController extends Controller
 
     public function store()
     {
-        $attributes = request()->validate([
-            'title' => 'required',
-            'description' => 'required|max:100',
-            'notes' => 'max:255'
-        ]);
-
-        $project = Auth::user()->projects()->create($attributes);
-
-        return redirect($project->path());
-    }
-
-    public function update(Project $project)
-    {
-        $this->authorize('update', $project);
-
-        $project->update(request(['notes']));
+        $project = Auth::user()->projects()->create($this->validateRequest());
 
         return redirect($project->path());
     }
@@ -46,5 +31,28 @@ class ProjectsController extends Controller
     public function create()
     {
         return view('projects.create');
+    }
+
+    public function edit(Project $project)
+    {
+        return view('projects.edit', compact('project'));
+    }
+
+    public function update(Project $project)
+    {
+        $this->authorize('update', $project);
+
+        $project->update($this->validateRequest());
+
+        return redirect($project->path());
+    }
+
+    protected function validateRequest()
+    {
+        return request()->validate([
+            'title' => 'required',
+            'description' => 'required|max:100',
+            'notes' => 'max:255'
+        ]);
     }
 }
--- tests/Feature/ManageProjectsTest.php
+++ tests/Feature/ManageProjectsTest.php
@@ -21,6 +21,7 @@ class ManageProjectsTest extends TestCase
         $this->get('/projects')->assertRedirect('login');
         $this->get('/projects/create')->assertRedirect('login');
         $this->get($project->path())->assertRedirect('login');
+        $this->get($project->path() . '/edit')->assertRedirect('login');
         $this->post('/projects', $project->toArray())->assertRedirect('login');
     }
 
@@ -56,8 +57,13 @@ class ManageProjectsTest extends TestCase
             ->ownedBy($this->signIn())
             ->create();
 
-        $this->patch($project->path(), $attributes = ['notes' => 'Changed'])
-            ->assertRedirect($project->path());
+        $this->patch($project->path(), $attributes = [
+            'title' => 'Changed',
+            'description' => 'Changed',
+            'notes' => 'Changed'
+        ])->assertRedirect($project->path());
+
+        $this->get($project->path() . '/edit')->assertOk();
 
         $this->assertDatabaseHas('projects', $attributes);
     }
