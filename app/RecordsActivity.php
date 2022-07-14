e58be605febf0b035c76dd4d11d6237417ed0c37
johnsci911 <johnguitarizta@gmail.com>
2022-07-15 04:32:41 +0800

    Refactor to remove duplicates

--- app/RecordsActivity.php
+++ app/RecordsActivity.php
@@ -1,18 +1,38 @@
-<?php
+<?php declare(strict_types = 0);
 
 namespace App;
 
 trait RecordsActivity
 {
-    public $oldAttribute = [];
+    public $oldAttributes = [];
 
     public static function bootRecordsActivity()
     {
-        static::updating(function ($model) {
-            $model->oldAttribute = $model->getOriginal();
-        });
+        foreach (self::recordableEvents() as $event) {
+            static::$event(function ($model) use ($event) {
+                $model->recordActivity($model->activityDescription($event));
+            });
 
+            if ($event === 'updated') {
+                static::updating(function ($model) {
+                    $model->oldAttributes = $model->getOriginal();
+                });
+            }
+        }
+    }
 
+    protected function activityDescription($description)
+    {
+        return "{$description}_" . strtolower(class_basename($this));
+    }
+
+    protected static function recordableEvents()
+    {
+        if (isset(static::$recordableEvents)) {
+            return static::$recordableEvents;
+        }
+
+        return ['created', 'updated', 'deleted'];
     }
 
     public function recordActivity($description)
@@ -33,7 +53,7 @@ trait RecordsActivity
     {
         if ($this->wasChanged()) {
             return [
-                'before' => array_diff($this->oldAttribute, array_except($this->getAttributes(), 'updated_at')),
+                'before' => array_diff($this->oldAttributes, array_except($this->getAttributes(), 'updated_at')),
                 'after' => array_except($this->getChanges(), 'updated_at')
             ];
         }
