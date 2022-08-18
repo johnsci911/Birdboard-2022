10526abe8d34fc9f37ac6edf0688179b83f55a12
johnsci911 <johnguitarizta@gmail.com>
2022-08-18 18:39:41 +0800

    Docblocks

--- app/RecordsActivity.php
+++ app/RecordsActivity.php
@@ -4,8 +4,16 @@ namespace App;
 
 trait RecordsActivity
 {
+	/**
+	 * The project's old attributes.
+	 *
+	 * @var array
+	 **/
     public $oldAttributes = [];
 
+	/**
+	 * Boot the trait
+	 **/
     public static function bootRecordsActivity()
     {
         foreach (self::recordableEvents() as $event) {
