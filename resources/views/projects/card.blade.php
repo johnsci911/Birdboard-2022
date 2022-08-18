247c0997e1a13ae3f8484bbc4987047d7eed658d
johnsci911 <johnguitarizta@gmail.com>
2022-08-19 01:40:01 +0800

    Auth users can delete projects

--- resources/views/projects/card.blade.php
+++ resources/views/projects/card.blade.php
@@ -2,5 +2,15 @@
   <h3 class="font-normal text-xl py-4 -ml-5 border-l-4 border-blue-light pl-4 mb-3">
     <a href="{{ $project->path() }}" class="text-black no-underline">{{ $project->title }}</a>
   </h3>
-  <div class="text-grey">{{ str_limit($project->description, 250) }}</div>
+
+  <div class="text-grey mb-8">{{ str_limit($project->description, 250) }}</div>
+
+  <footer>
+    <form method="POST" action="{{ $project->path() }}" class="text-right">
+      @method('DELETE')
+      @csrf
+
+      <button type="submti" class="text-xs">Delete</button>
+    </form>
+  </footer>
 </div>
